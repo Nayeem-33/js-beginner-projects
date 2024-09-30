6f3dfb6a90a7f92f9e071933bea6a67e12d0f2a0
Nayeem Uddin <unayeem33@gmail.com>
2024-09-30 21:34:51 +0600

    web

--- track-expence-app-DOM/js/index.js
+++ track-expence-app-DOM/js/index.js
@@ -1,1 +1,61 @@
 const expenseList = document.getElementById("expense-list");
+
+let expenceObjArr = [];
+
+let filteredArray
+
+// Add input as aobject in array and account balance
+document.getElementById("submit-expence").addEventListener("click", function (event) {
+    event.preventDefault();
+    // Creating New Object: Selecting Value
+    let id = Date.now()
+    let description = idValue("description")
+    let amount = idAmount("amount")
+    let category = idValue("category")
+    // Creating New Object: Creating & Push
+    const newObject = { id, description, amount, category }
+    expenceObjArr.push(newObject)
+
+    // Clear Form
+    document.getElementById('description').value = '';
+    document.getElementById('amount').value = '';
+    document.getElementById('category').value = 'other';
+
+    calculateExpences(expenceObjArr)
+
+    fatchList(expenceObjArr)
+})
+
+// filter Array - Filter By Category
+document.getElementById("filter-category").addEventListener("change", function (event) {
+    event.preventDefault()
+    let filterValue = idValue("filter-category")
+    // sss
+    console.log(filterValue)
+
+    if (filterValue === "food") {
+        filteredArray = filterArray(expenceObjArr, "food")
+        fatchList(filteredArray)
+    } else if (filterValue === "shopping") {
+        filteredArray = filterArray(expenceObjArr, "shopping")
+        fatchList(filteredArray)
+    } else if (filterValue === "grocery") {
+        filteredArray = filterArray(expenceObjArr, "grocery")
+        fatchList(filteredArray)
+    } else if (filterValue === "travel") {
+        filteredArray = filterArray(expenceObjArr, "travel")
+        fatchList(filteredArray)
+    } else if (filterValue === "entertainment") {
+        filteredArray = filterArray(expenceObjArr, "entertainment")
+        fatchList(filteredArray)
+    } else if (filterValue === "other") {
+        filteredArray = filterArray(expenceObjArr, "other")
+        fatchList(filteredArray)
+    } else{
+        fatchList(expenceObjArr)
+    }
+
+
+})
+
+// fatch object - 
--- track-expence-app-DOM/js/utilities.js
+++ track-expence-app-DOM/js/utilities.js
@@ -1,1 +1,59 @@
-console.log("uti")
+// Global Var
+let selectFilter = document.getElementById("filter-category").value
+
+// id to value & id to number
+const idValue = (id) => document.getElementById(id).value
+const idAmount = (id) => Number(document.getElementById(id).value)
+
+const balanceDisplay = document.getElementById('total-amount')
+
+// Calculate Total Expence Reduce
+const calculateExpences = (arrayOfExpence) => balanceDisplay.innerHTML = arrayOfExpence.reduce((sum, object) => sum + object.amount, 0).toFixed(2)
+
+// filter the object
+let filterArray = (expenceArray, cata) => newArray = expenceArray.filter((object) => object.category === cata);
+
+
+// Fatch Function
+function fatchList(array) {
+    expenseList.innerHTML = "";
+
+    let iconClass;
+    if (category === "food") {
+        iconClass = "fa-solid fa-utensils";
+    } else if (category === "shopping") {
+        iconClass = "fa-solid fa-cart-shopping";
+    } else if (category === "grocery") {
+        iconClass = "fa-solid fa-basket-shopping";
+    } else if (category === "travel") {
+        iconClass = "fa-solid fa-route";
+    } else if (category === "entertainment") {
+        iconClass = "fa-solid fa-film";
+    } else {
+        iconClass = "fa-solid fa-cash-register";
+    }
+
+    // Create Element
+    array.forEach(element => {
+        let newLi = document.createElement('li')
+        newLi.classList.add("flex", "gap-5", "py-2", "px-4", "bg-white", "rounded-md", "shadow", "items-center");
+        newLi.setAttribute('id', `expense-${element.id}`);
+        newLi.innerHTML = `
+        <div class="w-12 h-12 bg-blue-600 text-white p-4 rounded-2xl flex justify-center items-center">
+            <i class="${iconClass} text-2xl"></i>
+        </div>
+        <div class="w-full flex justify-between items-center">
+        <div>
+            <p><span class="font-semibold">Amount:</span> ৳ ${element.amount}</p>
+            <p><span class="font-semibold">Description:</span> ${element.description}</p>
+            <p><span class="font-semibold">Date:</span> ${new Date().toLocaleDateString()}</p>
+        </div>
+        <div>
+        <button id="delete-${element.id}" class="text-red-500 border border-red-500 text-white px-2 py-1 rounded-md"><i class="fa-regular fa-trash-can"></i></button>
+        </div>
+        </div>
+        `
+        expenseList.appendChild(newLi);
+    });
+
+}
